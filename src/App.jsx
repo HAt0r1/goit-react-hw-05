86fe72fb7b97fcf73535b244b8b5e995fa014aeb
Hatori <ahgangadm@gmail.com>
2024-04-27 21:55:30 +0300

    Create main component with navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,36 @@
-const App = () => {};
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import LayoutApp from "./components/LayoutApp/LayoutApp";
+import Fallback from "./components/Fallback/Fallback";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+
+// Lazy import
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
+const MovieDetailsPage = lazy(() =>
+  import("./pages/MovieDetailsPage/MovieDetailsPage")
+);
+const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
+const MovieReviews = lazy(() =>
+  import("./components/MovieReviews/MovieReviews")
+);
+
+const App = () => {
+  return (
+    <LayoutApp>
+      <Suspense fallback={<Fallback />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </LayoutApp>
+  );
+};
 
 export default App;
