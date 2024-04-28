a838961cabf2955811958a478c62905efab01696
Hatori <ahgangadm@gmail.com>
2024-04-28 18:34:20 +0300

    Rewrite API

--- src/movies-api.js
+++ src/movies-api.js
@@ -3,10 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://api.themoviedb.org/3";
 
 const options = {
-  params: {
-    language: "en-US",
-  },
-  include_adult: false,
   headers: {
     Authorization:
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhN2NjZGFjNjY1ZmQ1ZmZhZTZhZjRiZDI5YTM1MjRhNCIsInN1YiI6IjY2MmNmNmExOGE4OGIyMDEyNGNkZDIwMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vK9OZiKRB771fp8uzNdjTb0qVQ0OGuyjX7Q46g6AIUg",
@@ -14,12 +10,17 @@ const options = {
 };
 
 export const getTrendMovies = async () => {
-  const responce = await axios.get("trending/movie/day", options);
+  const responce = await axios.get(
+    "trending/movie/day?language=en-US",
+    options
+  );
   return responce.data;
 };
 
 export const getMoviesByKeyWords = async (word) => {
-  options.params.query = word;
-  const responce = await axios.get("search/movie", options);
+  const responce = await axios.get(
+    `search/movie?query=${word}&language=en-US`,
+    options
+  );
   return responce.data;
 };
