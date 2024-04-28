ffcd5275449a3b830c7a951c6afa65f717d3842b
Hatori <ahgangadm@gmail.com>
2024-04-28 18:34:33 +0300

    Update MoviewsPage component

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { Formik, Form, Field } from "formik";
 import { getMoviesByKeyWords } from "../../movies-api";
+import { useSearchParams } from "react-router-dom";
 import MovieList from "../../components/MovieList/MovieList";
 import Error from "../../components/Error/Error";
 import Loader from "../../components/Loader/Loader";
@@ -11,46 +11,46 @@ const MoviePage = () => {
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const [keyWord, setKeyWord] = useState(null);
+
+  const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
-    async function getSearchMovies() {
-      try {
-        setIsLoading(true);
-        if (keyWord.key === "") {
-          return;
+    async function getMovies() {
+      if (searchParams.get("query" !== null)) {
+        try {
+          setIsLoading(true);
+          const data = await getMoviesByKeyWords(searchParams.get("query"));
+          setMovies(data.results);
+        } catch (error) {
+          setError(true);
+        } finally {
+          setIsLoading(false);
         }
-        const data = await getMoviesByKeyWords(keyWord.key);
-        setMovies(data.results);
-      } catch (error) {
-        setError(true);
-      } finally {
-        setIsLoading(false);
       }
     }
-    getSearchMovies();
-  }, [keyWord]);
+    getMovies();
+  }, [searchParams]);
 
-  const handleSubmit = (values, action) => {
-    if (values.key === "") {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const keyWord = form.movie.value.trim();
+    if (keyWord === "") {
       return;
     }
-    setKeyWord({ key: values.key });
-    action.resetForm();
+    setSearchParams({ query: keyWord });
   };
 
   return (
     <main className={style.main}>
-      <Formik initialValues={{ key: "" }} onSubmit={handleSubmit}>
-        <Form className={style.form}>
-          <Field className={style.input} type="text" name="key" />
-          <button className={style.button} type="submit">
-            Search
-          </button>
-        </Form>
-      </Formik>
+      <form className={style.form} onSubmit={handleSubmit}>
+        <input className={style.input} type="text" name="movie" id="" />
+        <button className={style.button} type="submit">
+          Send
+        </button>
+      </form>
       {movies.length > 0 && <MovieList movies={movies} />}
-      {/* {(movies.length > 0 || error) && <Error />} */}
+      {error && <Error />}
       {isLoading && <Loader />}
     </main>
   );
